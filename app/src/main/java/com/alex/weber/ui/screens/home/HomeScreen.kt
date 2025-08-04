191a0a722a3c4c1f3d6e5af4e095a6ce7d92e6e5
midmc <silaisaac7@gmail.com>
2025-08-04 16:45:46 +0300

    my updates

--- app/src/main/java/com/alex/weber/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/alex/weber/ui/screens/home/HomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +15,8 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
@@ -26,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -34,27 +38,65 @@ import com.alex.weber.ui.screens.home.VideoPlayer
 
 @SuppressLint("UseKtx")
 @Composable
-fun Home(innerPadding : PaddingValues){
+fun Home(innerPadding: PaddingValues) {
 
     val context = LocalContext.current.applicationContext
 
+    data class Clip(
+        val title: String,
+        val description: String,
+        val image: Uri
+    )
 
-    Box(contentAlignment = Alignment.BottomEnd,
+    Box(
+        contentAlignment = Alignment.BottomEnd,
         modifier = Modifier
-        .fillMaxSize()
+            .fillMaxSize()
             .padding(innerPadding)
-        .background(Color.White),
-         ) {
-        Column {
-            VideoPlayer(uri = Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"))
+            .background(Color.White),
+    ) {
+
+        val clips = listOf<Clip>(
+            Clip(
+                "Laptop for sale",
+                "lenovo",
+                Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+            ),
+            Clip(
+                "Laptop for sale",
+                "lenovo",
+                Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4")
+            ),
+            Clip(
+                "Laptop for sale",
+                "lenovo",
+                Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+            )
+        )
+
+
+        LazyColumn {
+            itemsIndexed(clips) { index, item ->
+                VideoPlayer(title = item.title, description = item.description, item.image)
+//                VideoPlayer(
+//                    title = "Laptop for sale",
+//                    description = "Lenovo 4GB Ram",
+//                    Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+//                )
+            }
         }
+//        Column {
+//            VideoPlayer(title = "Laptop for sale", description = "Lenovo 4GB Ram", Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"))
+//            VideoPlayer(title = "Laptop for sale", description = "Lenovo 4GB Ram", Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"))
+//        }
 
 //     Right side interraction buttons
         Column(
             verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.End,
             modifier = Modifier
-                .padding(end = 16.dp, bottom = 80.dp).fillMaxHeight()
+                .padding(end = 16.dp, bottom = 80.dp)
+                .fillMaxHeight()
 //        verticalArrangement = Arrangement.spacedBy(20.dp),
 //        horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -109,12 +151,13 @@ fun Home(innerPadding : PaddingValues){
                     modifier = Modifier
                         .size(40.dp)
                         .clip(CircleShape)
-                        .border(2.dp, Color.Black, CircleShape).clickable(onClick = { })
+                        .border(2.dp, Color.Black, CircleShape)
+                        .clickable(onClick = { })
                 )
             }
         }
     }
-    }
+}
 
 
 
--- app/src/main/java/com/alex/weber/ui/screens/home/VideoPlayer.kt
+++ app/src/main/java/com/alex/weber/ui/screens/home/VideoPlayer.kt
@@ -2,19 +2,29 @@ package com.alex.weber.ui.screens.home
 
 import android.net.Uri
 import android.widget.FrameLayout
+import androidx.annotation.OptIn
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
+import org.w3c.dom.Text
 
+@OptIn(UnstableApi::class)
 @Composable
-fun VideoPlayer(uri: Uri){
+fun VideoPlayer(title: String, description: String, uri: Uri){
     val context = LocalContext.current
     val player = remember{
         ExoPlayer.Builder(context).build().apply {
@@ -22,22 +32,32 @@ fun VideoPlayer(uri: Uri){
             prepare()
             playWhenReady = true
         }
+
     }
-    Box(modifier = Modifier.fillMaxSize()){
+    Box(modifier = Modifier.fillMaxSize()
+        .pointerInput(Unit){
+            detectTapGestures(onTap = {
+                if (player.isPlaying){
+//                    Icon(imageVector = Icons.Default.Pause, contentDescription = "")
+                    player.pause()
+                }else{
+                    player.play()
+                }
+            })
+        }){
         AndroidView(
             factory = {
                 PlayerView(context).apply {
                     this.player = player
                     useController = false
+                    resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FILL
                     layoutParams = FrameLayout.LayoutParams(
                         FrameLayout.LayoutParams.MATCH_PARENT,
                         FrameLayout.LayoutParams.MATCH_PARENT
                     )
                 }
             },
-//            modifier = Modifier
-//                .fillMaxSize()
-//            .height(400.dp)
+
         )
     }
 
